da188bb4fe11d23b1666d62a7393ef2290791daa
Megh-Zyke <ms8615@srmist.edu.in>
2025-06-20 19:43:21 +0530

    deplot

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chidambaram-x-card-generator/",
+  base: "/chidambaram-x-card-generator",
   plugins: [react()],
 });
